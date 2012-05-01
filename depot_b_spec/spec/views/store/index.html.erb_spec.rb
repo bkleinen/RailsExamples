f6f66c6ce9d33446b6703811c95c58e43e210c7d
B Kleinen <kleinen@infrastructure.de>
2012-05-02 00:17:56 +0200

    Add Specs

--- depot_b_spec/spec/views/store/index.html.erb_spec.rb
+++ depot_b_spec/spec/views/store/index.html.erb_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe "store/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:products, [
+      stub_model(Product,
+        :title => "Title1",
+        :description => "MyText1",
+        :image_url => "ImageUrl.jpg",
+        :price => "9.99"
+      ),
+      stub_model(Product,
+        :title => "Title2",
+        :description => "MyText2",
+        :image_url => "ImageUrl.jpg",
+        :price => "9.99"
+      )
+    ])
+  end
+
+  it "renders a list of products" do
+    render
+    rendered.should have_content("Title1")
+    rendered.should have_selector('h3', name: "Title1")
+    rendered.should have_selector('h3', name: "Title2")
+    rendered.should have_selector('h3', count: 2)
+    rendered.should have_selector('.price', text: '9.99')
+    
+  end
 end
